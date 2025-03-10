fbf263ff6cfd02464baeb0599063ff32f32a57b3
WillyMonde <monde.willy.pro@gmail.com>
2025-03-10 14:50:32 +0100

    Init API

---
+++ src/main/java/com/bts/sio/azurimmoapi/AzurimmoApiApplication.java
@@ -0,0 +1,13 @@
+package com.bts.sio.azurimmoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzurimmoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzurimmoApiApplication.class, args);
+	}
+
+}
