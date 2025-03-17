693129162d0cc74460c0dc7420be50a9ab1fda51
WillyMonde <monde.willy.pro@gmail.com>
2025-03-17 11:42:30 +0100

    Repository Contrat

---
+++ src/main/java/com/bts/sio/azurimmoapi/repository/PaiementRepository.java
@@ -0,0 +1,10 @@
+package com.bts.sio.azurimmoapi.repository;
+
+
+import com.bts.sio.azurimmoapi.model.Paiement;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaiementRepository  extends JpaRepository<Paiement, Long> {
+}
